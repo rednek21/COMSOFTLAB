31b5f187aabd8f8251caf2c7197a93e099397529
rednek21 <egor.podustov@mail.ru>
2024-08-05 20:28:12 +0300

    add progress-bar + small refactoring))

---
+++ emails/api/views.py
@@ -0,0 +1,44 @@
+import logging
+from rest_framework import status, viewsets
+from rest_framework.response import Response
+
+from transport.imap.imap_client import IMAPClient
+from transport.imap.utils import process_email_message, send_email_notification
+from emails.models import EmailConfig, EmailMessage
+from emails.serializers import EmailMessageSerializer
+
+logger = logging.getLogger(__name__)
+
+
+class FetchEmailsView(viewsets.ViewSet):
+    def list(self, request):
+        config = EmailConfig.objects.first()
+        if not config:
+            return Response({'error': 'Email configuration not found.'}, status=status.HTTP_404_NOT_FOUND)
+
+        imap_client = IMAPClient(config.server, config.port, config.email, config.password)
+        imap_client.login()
+        imap_client.select('inbox')
+
+        last_email = EmailMessage.objects.all().first()
+        since_date = last_email.date_sent if last_email else None
+        email_ids = imap_client.search_emails(since_date)
+
+        messages = []
+        for email_id in email_ids:
+            msg = imap_client.fetch_email(email_id)
+            email_message, attachments = process_email_message(msg, imap_client)
+            if email_message:
+                send_email_notification(email_message)
+                messages.append(email_message)
+
+        serializer = EmailMessageSerializer(messages, many=True)
+        return Response({
+            "total": len(messages),
+            "data": serializer.data
+        })
+
+
+class EmailListView(viewsets.ReadOnlyModelViewSet):
+    queryset = EmailMessage.objects.all()
+    serializer_class = EmailMessageSerializer
--- emails/urls.py
+++ emails/urls.py
@@ -1,10 +1,6 @@
 from django.urls import path
-from .api import FetchEmailsView, EmailListView
 from .views import EmailsView
 
 urlpatterns = [
-    path('fetch-emails/', FetchEmailsView.as_view(), name='fetch_emails'),
-    path('emails-list/', EmailListView.as_view(), name='email_list'),
-
     path('emails/', EmailsView.as_view(), name='emails'),
 ]
